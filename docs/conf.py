e866add84fc19ce7545342c7e5321c142bacd3ce
PK <pkjmesra@gmail.com>
2023-07-16 02:18:05 +0200

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ from datetime import date
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
 
-from classes.Changelog import VERSION  # noqa: E402
+from src.classes import VERSION  # noqa: E402
 
 # -- General configuration -----------------------------------------------------
 
